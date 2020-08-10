b92f9ad00f008d4a34f15569d180684730793a91
Adi-V05 <dizahagi@gmail.com>
2020-08-10 20:19:45 +0700

    add header

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,15 @@
-<h1>CHARITY</h1>
+<script>
+let tittle = "charity";
+setTimeout (function () {
+	tittle = "Donation";
+}, 2000) ;
+</script>
+
+<h1>{tittle}</h1>
+
+<style>
+h1 {
+	font-size: 3em;
+	color: brown;
+}
+</style>
