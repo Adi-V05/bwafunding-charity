16df687d86da634ed6c2e6af43fe8cc79491a921
Adi-V05 <dizahagi@gmail.com>
2020-08-11 20:24:03 +0700

    comp

---
+++ src/components/Charitylist.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let charities = ['Default Charity 1'];
+</script>
+<div>
+    <h2>Daftar Charity</h2>
+    <pre>{charities}</pre>
+    <ul>
+    <li>Daftar 1</li>
+    <li>Daftar 2</li>
+</ul>
+ </div>
