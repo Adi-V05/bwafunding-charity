608ab162304df9bde4c8cdb466df68e5a6c51e2b
Adi-V05 <dizahagi@gmail.com>
2020-08-12 00:45:29 +0700

    Home

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,5 @@
 <script>
-    import CharityList from "./CharityList.svelte";
-
-
-    let title = "Charity";
-    setTimeout(function (){
-        title = "Donation";
-    }, 2000);
-
-    let charities = ['Charity 1','Charity 2','Charity 3'];
+    import Home from './Pages/Home.svelte';
 </script>
 
-<style>
-h1 {
-    font-size: 3em;
-    color: blue;
-}
-:global(div) {
-    border: 0.5em solid blueviolet;
-    padding: 1em;
-}
-</style>
-
-<div>
-    <h1>{title}</h1>
-    <CharityList {charities}/>
-</div>
-
+<Home />
