5455f37470bd5903a20d92813390e817d20cbd5f
Adi-V05 <dizahagi@gmail.com>
2020-08-11 20:22:34 +0700

    comp

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,5 @@
 <script>
-
-import Charitylist from "./Charitylist.svelte";
-
-let tittle = "Charity";
-setTimeout(function () {
-tittle = "Donation";
-}, 2000);
-
-let charities = ['Charity 1','Charity 2','Charity 3'];
+    import home from './pages/home.svelte';
 </script>
 
-<style>
-    h1 {
-        font-size: 3em;
-        color:brown;
-        }
-        :global(div) {
-            border: 0.5em solid blue;
-            padding: 1em;
-        }
-</style>
-
-<div>
-<h1>{tittle}</h1>
-<Charitylist {charities}/>
-</div>
+<home />
--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -1,19 +1,21 @@
 <script>
 
-    import CharityList from "..component/Charitylist.svelte";
+    import Charitylist from "../components/Charitylist.svelte";
     
-    let tittle = "charity";
-    let charities = ['Charity 1','Charity 2', 'Charity 3'];
+    let tittle = "Charity";
+    
+    let charities = ['Charity 1','Charity 2','Charity 3'];
     </script>
     
     <style>
-    h1 {
-        font-size: 3em;
-        color: brown;
-    }
+        h1 {
+            font-size: 3em;
+            color:brown;
+            }
     </style>
+    
     <div>
-        <h1>{tittle}</h1>
-        <CharityList charities = {charities}/>
-        <CharityList />
+    <h1>{tittle}</h1>
+    <Charitylist {charities}/>
     </div>
+    
