5d278713268334c2306d95391d1fc83ae55dd3b2
Adi-V05 <dizahagi@gmail.com>
2020-08-11 14:31:49 +0700

    padding

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,18 @@
 <script>
 
 import Charitylist from './Charitylist.svelte';
-
 let tittle = "charity";
 setTimeout (function () {
 	tittle = "Donation";
 }, 2000) ;
 </script>
 
-<h1>{tittle}</h1>
-<Charitylist/>
 <style>
 h1 {
 	font-size: 3em;
 	color: brown;
 }
-:global (div) {
+:global(div) {
 	border: 0.5em solid black;
     padding: 1em;
 }
