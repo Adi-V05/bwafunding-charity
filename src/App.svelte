3089cc938e68bf30f4c8bd7f0145faea03db2d01
Adi-V05 <dizahagi@gmail.com>
2020-08-11 18:28:11 +0700

    charity

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,20 @@
 <script>
-import Home from './pages/Home.svelte';
+import CharityList from ".CharityList.svelte";
+import Charitylist from "./Charitylist.svelte";
+
+let tittle = "Charity";
+setTimeout(function () {
+tittle = "Donation";
+}, 2000);
 </script>
 
-<Home />
+<style>
+    h1 {
+        font-size: 3em;
+        color:brown;
+        }
+</style>
+
+<h1>{tittle}</h1>
+<Charitylist/>
+
---
+++ src/Charitylist.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let charities;
+</script>
+
+<div>
+    <h2>Daftar Charity</h2>
+    {#if charities !==undefined}
+     <pre>{charities}</pre>
+<ul>
+    <li>Daftar 1</li>
+    <li>Daftar 2</li>
+</ul>
+{/if}
+</div>
