3ade1a3b533442ce6a7603051e9d7707589fca2e
Adi-V05 <dizahagi@gmail.com>
2020-08-11 16:22:17 +0700

    modif

---
+++ src/component/Charitylist.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let charities = ['local charity'];
+</script>
+<div>
+    <h2>Daftar Charity</h2>
+     <pre>{charities}</pre>
+<ul>
+    <li>Daftar 1</li>
+    <li>Daftar 2</li>
+</ul>
+</div>
---
+++ src/pages/home.svelte
@@ -0,0 +1,19 @@
+<script>
+
+    import Charitylist from '../component/Charitylist.svelte';
+    
+    let tittle = "charity";
+
+    let charities = ['Charity 1','Charity 2', 'Charity 3'];
+    </script>
+    
+    <style>
+    h1 {
+        font-size: 3em;
+        color: brown;
+    }
+    </style>
+    <div>
+        <h1>{tittle}</h1>
+        <Charitylist charities = {charities}/>
+    </div>
